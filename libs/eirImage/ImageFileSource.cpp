8afabceb857456cf9bf46cc6e92f529f8af4e69a
Tony Otto EIRC <eclipseir.tony@gmail.com>
2025-10-13 00:06:57 -0700

    More ImageFileSource::pulse().

--- libs/eirImage/ImageFileSource.cpp
+++ libs/eirImage/ImageFileSource.cpp
@@ -22,9 +22,10 @@ void ImageFileSource::clear()
         mpPulseTimer->deleteLater();
         mpPulseTimer = nullptr;
     }
+    mFlags = $null;
     mRootDir = QDir();
-    mCurrentIndex = QModelIndex();
-    mFIQueue.clear();
+    mFetchIndex = QModelIndex();
+    mFetchQueue.clear();
     mImageQueue.clear();
 }
 
@@ -50,11 +51,33 @@ void ImageFileSource::start(const QDir &rootDir)
 void ImageFileSource::fetch()
 {
     FNSLOT();
-
+    fetchMore(mFetchIndex);
+    mFlags |= Fetching;
 }
 
 void ImageFileSource::pulse()
 {
     FNSLOT();
-
+    if (mFlags & Fetching)
+        pulseFetch();
 }
+
+void ImageFileSource::pulseFetch()
+{
+    if (mFetchQueue.count() > 32)   return;
+
+    int tCount = 16;
+    while (mFetchIndex.row() < rowCount(mFetchIndex) && tCount)
+    {
+        const QFileInfo cFI = fileInfo(mFetchIndex);
+        mFetchIndex = index(1 + mFetchIndex.row(),
+                            mFetchIndex.column(),
+                            mFetchIndex);
+        --tCount;
+    }
+
+    if (mFetchIndex.row() >= rowCount(mFetchIndex))
+        mFlags &= ~ Fetching;
+}
+
+
--- libs/eirImage/ImageFileSource.h
+++ libs/eirImage/ImageFileSource.h
@@ -15,6 +15,13 @@ class EIRIMAGE_EXPORT ImageFileSource : public QFileSystemModel
 {
     Q_OBJECT
 public: // types
+    enum Flag
+    {
+        $null = 0,
+        Fetching        = 0x0001,
+    };
+    Q_DECLARE_FLAGS(Flags, Flag)
+    Q_FLAG(Flags)
 
 public: // ctors
     explicit ImageFileSource(QObject *parent = nullptr);
@@ -22,13 +29,15 @@ public: // ctors
 public slots:
     void clear();
     void start(const QDir &rootDir);
+
+private slots:
     void fetch();
     void pulse();
-
+    void pulseFetch();
 
 signals:
     void starting(const QDir &dir);
-    void fetched(const int n);
+    void fetched(const int fetchQueueCount);
 
 public: // const
 
@@ -39,8 +48,9 @@ public: // pointers
 
 private:
     QTimer * mpPulseTimer=nullptr;
+    Flags mFlags = $null;
     QDir mRootDir;
-    QModelIndex mCurrentIndex;
-    QQueue<QFileInfo> mFIQueue;
+    QModelIndex mFetchIndex;
+    QQueue<QFileInfo> mFetchQueue;
     QQueue<ColorImage> mImageQueue;
 };
